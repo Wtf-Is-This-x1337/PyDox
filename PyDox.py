078948839df8b19e43514363bbb59c17a68232ad
Andrei <78364462+Wtf-Is-This-x1337@users.noreply.github.com>
2021-06-29 07:11:32 -0500

    Update PyDox.py

--- PyDox.py
+++ PyDox.py
@@ -76,16 +76,40 @@ def AutoDox():
 
 def RealName():
     logo()
-    print(f'''{ngreen} Dox using a name -\n\n{green}''')
+    print(f'''{ngreen}Dox using a real name -\n\n{green}''')
     goback()
+
+def username():
+    logo()
+    print(f'''{ngreen}Dox using a username - \n\n{green}''')
+    goback()
+
+def phone():
+    logo()
+    print(f'''{ngreen}Dox using a username - \n\n{green}''')
+    goback()
+
+def address():
+    logo()
+    print(f'''{ngreen}Dox using an address - \n\n{green}''')
+    goback()
+
+def tools():
+    logo()
+    print(f'''{ngreen}Dox using a username - \n\n{green}''')
+    goback()
+
+def createList():
+    logo()
+    print(f'''{ngreen}Create a list with all the informations - \n\n{green}''')
 
 def main():
 	logo()
 	select = input(f'''{green}\nSelect:
 	1) {ngreen}Help\t\t{green}2) {ngreen}Github\t  {green}3) {ngreen}Credits\n
-	{green}4) {ngreen}Settings\t{green}5) {ngreen}Set API keys\t  {green}6) {ngreen}Auto Dox\n
-    {green}\t7) {ngreen}RealName\t{green}8) {ngreen}Username\t  {green}9) {ngreen}Phone\n
-    {green}\t10) {ngreen}Address\t{green}11) {ngreen}Tools\t  {green}12) {ngreen}Create list\n
+	{green}4) {ngreen}Settings\t{green}5) {ngreen}Set API Keys\t  {green}6) {ngreen}Auto Dox\n
+    {green}\t7) {ngreen}Real Name\t{green}8) {ngreen}Username\t  {green}9) {ngreen}Phone\n
+    {green}\t10) {ngreen}Address\t{green}11) {ngreen}Tools\t {green}12) {ngreen}Create List\n
                         {green}13) {ngreen}Quit
 	\n{green}Option: ''')
 
@@ -102,6 +126,21 @@ def main():
 		SetApi()
 	elif select == '6':
 		AutoDox()
+	elif select == '7':
+		RealName()
+	elif select == '8':
+		username()
+	elif select == '9':
+		phone()
+	elif select == '10':
+		address()
+	elif select == '11':
+		tools()
+	elif select == '12':
+		createList()
+	elif select == '13':
+		clear()
+		sys.exit()
 	else:
 		clear()
 		sys.exit('Invalid Input')
